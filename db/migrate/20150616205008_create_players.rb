a626e9c31c9f2ea7f9a3597fd50456b0c444d8a0
Katye Russell <katye.russell@gmail.com>
2015-06-16 17:18:02 -0500

    Rails generate player model.

---
+++ db/migrate/20150616205008_create_players.rb
@@ -0,0 +1,14 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :cell
+      t.integer :points
+      t.integer :negs
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615215323) do
+ActiveRecord::Schema.define(version: 20150616205008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "cell"
+    t.integer  "points"
+    t.integer  "negs"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",            null: false
