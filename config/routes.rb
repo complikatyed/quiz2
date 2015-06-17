0476462ecfb875ea215bfbec7df80de4daae8279
Katye Russell <katye.russell@gmail.com>
2015-06-17 15:51:44 -0500

    Add methods, controllers, and views for creating new players.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'landing#index'
 
+  resources :players, only: [:new, :create, :show, :destroy]
   resource :user_session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
--- spec/features/user_creates_player_spec.rb
+++ spec/features/user_creates_player_spec.rb
@@ -1,35 +1,33 @@
-# feature "user creates player profile" do
-#   # As a user
-#   # In order to represent my student players in the game
-#   # I want to create a player.
+feature "user creates player profile" do
+  # As a user
+  # In order to represent my student players in the game
+  # I want to create a player.
 
-#   # Acceptance Criteria:
-#   # * Player must have name, email, cell-phone.
-#   # * Player must be publicly visible once saved <-- not sure how this should look
+  # Acceptance Criteria:
+  # * Player must have name, email, cell-phone, points, and negs.
+  # * Player must be publicly visible once saved <-- not sure how this should look
 
-#   scenario "logged out users can't create players" do
-#     visit home_path
-#     page.should_not have_content("create my player profile")
-#     visit new_player_path
-#     should_be_denied_access
-#   end
 
-#   scenario "happy path create player profile" do
-#     me = Fabricate(:user, name: "Sarah")
-#     signin_as me
-#     click_on "create my player profile"
-#     fill_in "First Name", with: "Sarah"
-#     fill_in "Last Name", with: "Harrison"
-#     fill_in "Email", with: "sah@email.com"
-#     fill_in "Cell", with: "615-555-1212"
-#     click_on "save my profile"
-#     page.should have_notice("Your player profile has been saved.")
-#     current_path.should == player_path/index
-#     page.should have_link "Sarah H"
-#     click_on "Sarah H"
-    
-#     page.should have_css("h4", text: "Sarah Harrison")
-#     page.should have_css("p", text: "sah@email.com")
-#     page.should have_css("p", text: "615-555-1212")
-#   end
+  scenario "happy path create player profile" do
+    me = Fabricate(:user, name: "Sarah")
+    signin_as me
+    click_on "I need to create my player profile"
+    page.should have_content("First name *")
+    page.should have_content("Last name *")
+    page.should have_content("Email *")
+    page.should have_content("Cell")
+    fill_in "First name", with: "Sarah"
+    fill_in "Last name", with: "Harrison"
+    fill_in "Email", with: "sah@email.com"
+    fill_in "Cell", with: "6155551212"
+    click_on "I'm ready to play!"
+    within("h4") do
+      page.should have_content("Sarah H")
+    end
+    page.should have_content("Email: sah@email.com")
+    page.should have_content("Cell: 6155551212")
+    page.should have_content("Points: 0")
+    page.should have_content("Negs: 0")
+  end
+end
 
