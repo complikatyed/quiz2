0a386c66e9f0e464f210626c44918f300fae6a50
Katye Russell <katye.russell@gmail.com>
2015-06-24 11:12:26 -0500

    Learned about button_to vs link_to and fixed POST route for create method.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,5 @@
 class PlayersController < ApplicationController
 
-  def index
-  end
-
   def new
     @player = Player.new
   end
@@ -10,38 +7,29 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_to player_path(@player),  notice: "Your player profile has been saved."
+      redirect_to player_path,  notice: "The player has been saved."
     else
       flash.alert = "Please fix the errors below to continue."
       render :new
     end
   end
 
-  def update
-    @user = current_user
-    @user.assign_attributes(user_params)
-    if @user.save
-      redirect_to user_posts_path(@user)
-      flash.notice = "Your profile has been updated"
-    else
-      flash.alert = "Please fix the errors below to continue"
-      render :edit
-    end
-  end
 
   def show
     @player = Player.find_by_id(params[:id])
   end
 
+
+  def index
+    @players = Player.all
+  end
+
+
     protected
+
 
   def player_params
     params.require(:player).permit(:first_name, :last_name, :email, :cell)
-  end
-
-  def destroy
-    logout
-    redirect_to root_path
   end
 
 end
